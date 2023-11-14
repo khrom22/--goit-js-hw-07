2ef17dba33a74b83b25af679e8ce4c7749a14734
Roman Khomyn <107980144+khrom22@users.noreply.github.com>
2023-11-14 22:05:31 +0200

    task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,6 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 
+const list = document.querySelector('.gallery')
+list.insertAdjacentHTML('beforeend', createMarkup())
 
+function createMarkup(galeryItems) {
+    return galleryItems.map(({ preview, original, description }) =>
+        `<li class="gallery__item">
+   <a class="gallery__link" href='${original}'>
+      <img class="gallery__image" src=${preview} alt=${description} />
+   </a>
+</li>`
+    ).join('')
+}
 
-console.log(galleryItems);
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+
+});
